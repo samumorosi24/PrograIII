08670a47e9942e65044a7eddd89313b433c662e2
samumorosi24 <161002084+samumorosi24@users.noreply.github.com>
2024-09-26 16:24:15 -0300

    complejidad algoritmica del ejercicio de cambio de moneda extranjera

---
+++ Clase4/Actividad2/ComplejidadAlgoritmica.java
@@ -0,0 +1,23 @@
+package Clase4.Actividad2;
+
+/*
+ * Complejidad Algoritmica para "cambio de moneda extranjera"
+ */
+
+ public class ComplejidadAlgoritmica {
+    /*
+     * Complejidad de la funcion encontrarMinimoComprobantes:
+     *
+     * 1. **Ordenacion de los Comprobantes**:
+     *    - Algoritmo de ordenacion, que toma O(n log n), donde n es el numero de comprobantes
+     *
+     * 2. **Seleccion de Comprobantes**:
+     *    - La busqueda y seleccion de comprobantes toma O(n) en el peor de los casos
+     *
+     * **Complejidad Total**:
+     * - La complejidad total es O(n log n) + O(n), lo que se simplifica a O(n log n)
+     *
+     * Esto significa que el tiempo de ejecución del algoritmo aumenta de manera logaritmica a medida que 
+     * aumentan los tipos de comprobantes
+     */
+}
