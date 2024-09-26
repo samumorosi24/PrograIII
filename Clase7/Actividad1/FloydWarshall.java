74ed3342fbbf8837a3858a47d86c11b9c180f3a7
samumorosi24 <161002084+samumorosi24@users.noreply.github.com>
2024-09-26 19:18:50 -0300

    ejercicio FloyWarshall de la clase 7

---
+++ Clase7/Actividad1/FloydWarshall.java
@@ -0,0 +1,54 @@
+package Clase7.Actividad1;
+
+public class FloydWarshall {
+    final static int INF = Integer.MAX_VALUE;  // Usamos el valor máximo de entero para representar el infinito
+    
+    public static void main(String[] args) {
+        FloydWarshall fw = new FloydWarshall();
+        int graph[][] = { 
+            {0, 8, 5},
+            {3, 0, INF},
+            {INF, 2, 0}
+        };
+        int V = graph.length;
+        fw.floydWarshall(graph, V);
+    }
+
+    void floydWarshall(int graph[][], int V) {
+        int dist[][] = new int[V][V];
+        
+        // Inicializar la matriz de distancias
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+        // Actualizar la matriz de distancias usando el algoritmo de Floyd-Warshall
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        // Imprimir la matriz de distancias
+        printSolution(dist, V);
+    }
+
+    void printSolution(int dist[][], int V) {
+        System.out.println("Matriz de distancias más cortas entre cada par de vértices:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF  ");
+                else
+                    System.out.print(dist[i][j] + "    "); // Espacio adicional para mejor alineación
+            }
+            System.out.println();
+        }
+    }
+}
